15e9bdf941fe7b4b9a1f21cb061689564c5fd684
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-17 15:51:01 +0000

    pushing the first ex1

--- week2/day1/exercise1.sql
+++ week2/day1/exercise1.sql
@@ -1,3 +1,32 @@
+-- Database: public
+
+-- DROP DATABASE IF EXISTS public;
+
+-- CREATE DATABASE public
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'fr-FR'
+--     LC_CTYPE = 'fr-FR'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+
+-- CREATE TABLE items(
+--  id serial PRIMARY KEY,
+--  name VARCHAR(255),
+--  price INTEGER 
+-- );
+
+-- CREATE TABLE customers(
+--  id serial PRIMARY KEY,
+--  first_name VARCHAR(255),
+--  last_name VARCHAR(255)
+-- );
+
+
 -- INSERT INTO "items" (name, price) VALUES ('Small Desk', 100), ('Large desk', 300), ('Fan', 80);
 -- INSERT INTO "customers" (first_name, last_name) VALUES ('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'), ('Trevor', 'Green'), ('Melanie', 'Johnson');
 
