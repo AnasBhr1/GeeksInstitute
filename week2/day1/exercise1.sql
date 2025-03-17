ac69090bff94b01f4d425655f22f37b36daded3b
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-17 15:47:38 +0000

    pushing the first ex1

---
+++ week2/day1/exercise1.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO "items" (name, price) VALUES ('Small Desk', 100), ('Large desk', 300), ('Fan', 80);
+-- INSERT INTO "customers" (first_name, last_name) VALUES ('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'), ('Trevor', 'Green'), ('Melanie', 'Johnson');
+
+-- select * from items
+-- select * from items where price > 80
+-- select * from items where price <= 300
+-- select * from customers where last_name = 'Smith'
+-- select * from customers where last_name = 'Jones'
+-- select * from customers where first_name != 'Scott'
+
+
+
+
