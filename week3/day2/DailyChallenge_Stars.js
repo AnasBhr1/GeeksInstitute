e355292a652f6097dbada0b9290e9f69cacbb2ad
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-25 23:24:51 +0000

    second challenge

---
+++ week3/day2/DailyChallenge_Stars.js
@@ -0,0 +1,16 @@
+// one loop 
+let pattern = "";
+for (let i = 1; i <= 6; i++) {
+  pattern += "* ";
+  console.log(pattern);
+}
+
+// nested loop
+for (let i = 1; i <= 6; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+  
