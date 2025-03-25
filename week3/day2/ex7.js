49022a389f158b66125effab46917de5e8fa6333
Anasbhr1 <anasbhr1@hotmail.com>
2025-03-25 15:31:41 +0000

    ex7

---
+++ week3/day2/ex7.js
@@ -0,0 +1,8 @@
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+const firstLetters = names.map(name => name[0]);
+
+firstLetters.sort();
+
+const secretSocietyName = firstLetters.join('');
+
+console.log(secretSocietyName);
